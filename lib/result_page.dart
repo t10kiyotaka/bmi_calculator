e939b85188ce832cb1b68a7557332293f1b19596
Kiyotaka <t10kiyotaka@gmail.com>
2020-09-22 11:00:57 +0900

    Make UI outline of result page

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,4 +1,6 @@
+import 'package:bmi_calculator/constant.dart';
 import 'package:flutter/material.dart';
+import 'reusable_card.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
@@ -7,7 +9,39 @@ class ResultsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Text('Hello'),
+      body: Column(
+        children: <Widget>[
+          Text(
+            'Your Result',
+            style: kTitleTextStyle,
+          ),
+          Expanded(
+            child: ReusableCard(
+              cardChild: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    'Normal',
+                    textAlign: TextAlign.center,
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '18.0',
+                    textAlign: TextAlign.center,
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'You\'re very normal!',
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
